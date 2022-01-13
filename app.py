8e8e187b28ae0fd3e236afaeee2e2275dbd69811
christer <christer.sundgren@systemkonstruktion.se>
2022-01-13 22:59:04 +0100

    Cleanup

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request
+import json
 
 app = Flask(__name__)
 
@@ -9,39 +10,26 @@ def hello_world():
 @app.route('/temperatur',methods=['GET','POST'])
 def hello_temp():
 
-
     if request.method == 'POST':
-        temperatur = request.json.get('temperatur')
-        password = request.json.get('password')
-        data = "{ 'temperatur': '" + temperatur + "' }\n"
-        f = open('config.json', 'w')
-        f.write(data)
+
+        filedata = str(request.json)
+        filedata = filedata.replace('\'','\"')
+        print (filedata)
+        f = open('temperatur.json', 'w')
+        f.write(filedata)
         f.close()
         return jsonify(isError= False,
                        message= "Success",
-                       statusCode= 200,
-                       data= data)
+                       statusCode= 200)
     else :
-        f = open('config.json', 'r')
+        f = open('temperatur.json', 'r')
         data = f.readline()
         f.close()
-        return data
+        print( data)
+        payload=json.loads(data)
+        return jsonify(payload)
 
-# allow both GET and POST requests
-@app.route('/form-example', methods=['GET', 'POST'])
-def form_example():
-    if request.method == 'POST':
-        language = request.form.get('temperatur')
-        framework = request.form.get('grader')
-        return '''
-                  <h1>The language value is: {}</h1>
-                  <h1>The framework value is: {}</h1>'''.format(language, framework)
-    return '''
-              <form method="POST">
-                  <div><label>Language: <input type="text" name="language"></label></div>
-                  <div><label>Framework: <input type="text" name="framework"></label></div>
-                  <input type="submit" value="Submit">
-              </form>'''
+
 
 if __name__ == '__main__':
     app.run(port=5000)
